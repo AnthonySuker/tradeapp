42b136ca79ca2720ea84980e15004bda5b965b6b
anthonysuker <anthony.suker@hotmail.com>
2020-12-09 15:51:39 +1100

    initial stock object commit

---
+++ stockObject.py
@@ -0,0 +1,37 @@
+class stockObject:
+    algoVars = {}
+    high = []
+    low = []
+    vol
+    close = []
+
+    def __init__ (self):
+        print("Created Base Object")
+
+    def importHigh(self, data):
+        self.high = data
+
+    def importLow(self, data):
+        self.low = data
+
+    def importClose(self, data):
+        self.close = data
+
+    def importVol(self, data):
+        self.Vol = data
+
+    def applyAlgo(self):
+        print("Base algorithm")
+
+
+
+### Child Stratagies ###
+
+class VolumeSpike(stockObject):
+    def applyAlgo(self):
+        print("doing stuff")
+        #if volume has increased by x% since last check
+        # & price has changed positively
+        #return true with confidence
+        
+
