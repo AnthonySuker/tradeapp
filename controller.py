a197f2fc73c798e9bf80c736520353039cf2e428
anthonysuker <anthony.suker@hotmail.com>
2020-12-09 16:40:40 +1100

    changes

---
+++ controller.py
@@ -0,0 +1,6 @@
+import stockObject
+
+
+# p1 = stockChecker('CBA.AX')
+
+# p1.printData()
--- stockObject.py
+++ stockObject.py
@@ -1,37 +1,41 @@
-class stockObject:
+import yfinance as yf
+
+def tttt():
+    print('tttt')
+
+class stockChecker:
     algoVars = {}
     high = []
     low = []
     vol = []
-    close = []
-
-    def __init__ (self):
+    close = []        
+    
+    def __init__ (self,name):
         print("Created Base Object")
-
-    def importHigh(self, data):
-        self.high = data
-
-    def importLow(self, data):
-        self.low = data
-
-    def importClose(self, data):
-        self.close = data
-
-    def importVol(self, data):
-        self.Vol = data
-
+        ticker = yf.Ticker(name)
+        hist = ticker.history(period='max')
+        self.high = hist['High']
+        self.low = hist['Low']
+        self.vol = hist['Volume']
+        self.close = hist['Close']
+        
+    def printData():
+        print(self.high)
+        print(self.low)
+        print(self.vol)
+        print(self.close)
+    
     def applyAlgo(self):
         print("Base algorithm")
-
+        
 
 
 ### Child Stratagies ###
 
-class VolumeSpike(stockObject):
+class VolumeSpike(stockChecker):
     def applyAlgo(self):
         print("doing stuff")
         #if volume has increased by x% since last check
         # & price has changed positively
         #return true with confidence
-        
 
